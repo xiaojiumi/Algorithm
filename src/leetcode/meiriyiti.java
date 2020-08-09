5d2b1ee73c0521eccd373d61144e2a2e83b443ee
song <1959084519@qq.com>
2020-08-09 22:44:31 +0800

    day 12

--- src/leetcode/meiriyiti.java
+++ src/leetcode/meiriyiti.java
@@ -264,4 +264,38 @@ public class meiriyiti {
         result[1]=left[0]+ right[0]+root.val;
         return result;
     }
+
+    public static void main(String[] args) {
+        meiriyiti m=new meiriyiti();
+        m.restoreIpAddresses("25525511135");
+    }
+
+
+    public List<String> restoreIpAddresses(String s) {
+        List<String> result=new ArrayList<>();
+        StringBuilder ip=new StringBuilder();
+        for (int a=1;a<4;a++){
+            for (int b=1;b<4;b++){
+                for (int c=1;c<4;c++){
+                    for (int d=1;d<4;d++){
+                        if (a+b+c+d==s.length()){
+                            int seg1=Integer.parseInt(s.substring(0,a));
+                            int seg2 = Integer.parseInt(s.substring(a, a + b));
+                            int seg3 = Integer.parseInt(s.substring(a + b, a + b + c));
+                            int seg4 = Integer.parseInt(s.substring(a + b + c, a + b + c + d));
+                            if (seg1<=255&&seg2<=255&&seg3<=255&&seg4<=255){
+                                ip.append(seg1).append(".").append(seg2).append(".").
+                                        append(seg3).append(".").append(seg4);
+                                if (ip.length()==s.length()+3)result.add(ip.toString());
+                                ip.delete(0,ip.length());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+
 }
