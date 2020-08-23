7f004965beb800f05a012992a781307c91a0d3b0
song <1959084519@qq.com>
2020-08-23 15:07:51 +0800

    day 19

--- src/leetcode/meiriyiti.java
+++ src/leetcode/meiriyiti.java
@@ -579,4 +579,11 @@ public class meiriyiti {
         }
         return false;
     }
+
+    public int rangeBitwiseAnd(int m, int n) {
+         while (m!=n){
+             n=n&(n-1);
+         }
+         return n;
+    }
 }
