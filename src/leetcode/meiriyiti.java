f80b5a2b320f70ab78fc5e4299408b45a06fea72
song <1959084519@qq.com>
2020-08-25 14:06:12 +0800

    day 20

--- src/leetcode/meiriyiti.java
+++ src/leetcode/meiriyiti.java
@@ -586,4 +586,76 @@ public class meiriyiti {
          }
          return n;
     }
+
+//    public boolean repeatedSubstringPattern(String s) {
+//        int n=s.length();
+//        for(int i=1;i<=n/2;i++){
+//            if (n%i==0){
+//                boolean flag=true;
+//                for (int j=i;j<n;j++){
+//                    if (s.charAt(j)!=s.charAt(j-i)){
+//                        flag=false;
+//                        break;
+//                    }
+//                }
+//                if(flag)return true;
+//            }
+//        }
+//        return false;
+//    }
+
+    public boolean repeatedSubstringPattern(String s) {
+        return (s+s).indexOf(s,1)!=s.length();
+    }
+
+    public static void main(String[] args) {
+        meiriyiti m=new meiriyiti();
+        List<List<Integer>> subsequences = m.findSubsequences(new int[]{4, 6, 6, 7});
+        System.out.println(subsequences);
+    }
+
+    List<Integer> temp = new ArrayList<Integer>();
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    Set<Integer> set = new HashSet<Integer>();
+    int n;
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        n= nums.length;;
+        for (int i=0;i<( 1<<n);i++){
+            findSubsequences(i,nums);
+            int hashValue = getHash(263, (int) 1E9 + 7);
+            if (check()&&!set.contains(hashValue)){
+                set.add(hashValue);
+                res.add(new ArrayList<>(temp));
+            }
+        }
+        return res;
+    }
+
+    public void findSubsequences(int mask, int[] nums) {
+        temp.clear();
+        for (int i = 0; i < n; ++i) {
+            if ((mask & 1) != 0) {
+                temp.add(nums[i]);
+            }
+            mask >>= 1;
+        }
+    }
+
+    public int getHash(int base, int mod) {
+        int hashValue = 0;
+        for (int x : temp) {
+            hashValue = hashValue * base % mod + (x + 101);
+            hashValue %= mod;
+        }
+        return hashValue;
+    }
+
+    public boolean check() {
+        for (int i = 1; i < temp.size(); ++i) {
+            if (temp.get(i) < temp.get(i - 1)) {
+                return false;
+            }
+        }
+        return temp.size() >= 2;
+    }
 }
