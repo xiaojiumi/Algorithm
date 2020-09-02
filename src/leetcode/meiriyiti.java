a48633f263d9a059a07f95c9166a7483fb372e80
song <1959084519@qq.com>
2020-09-02 13:15:18 +0800

    day 27

--- src/leetcode/meiriyiti.java
+++ src/leetcode/meiriyiti.java
@@ -749,4 +749,35 @@ public class meiriyiti {
         if (turn==1)return Math.max(scoreStart,scoreEnd);
         else return Math.min(scoreStart,scoreEnd);
     }
+
+    public boolean isNumber(String s) {
+        if(s==null||s.length()==0)return false;
+        boolean num=false,dot=false,e=false;
+        char[] str=s.trim().toCharArray();
+        for (int i=0;i<str.length;i++){
+            if (str[i]>='0'&&str[i]<='9'){
+                num=true;
+            }
+            else if(str[i]=='.'){
+                if(dot||e){
+                    return false;
+                }
+                dot=true;
+            }
+            else if (str[i]=='e'||str[i]=='E'){
+                if (!num||e){
+                    return false;
+                }
+                e=true;
+                num=false;
+            }
+            else if (str[i]=='-'||str[i]=='+'){
+                if (i!=0&&str[i-1]!='e'&&str[i-1]!='E')return false;
+            }
+            else {
+                return false;
+            }
+        }
+        return num;
+    }
 }
